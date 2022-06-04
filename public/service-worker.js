f84f96ffdc412663d68f457fec9578ebca81a27b
= <holdanmcmullen@gmail.com>
2022-06-04 03:43:44 -0600

    service worker fetches data

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,3 +47,24 @@ self.addEventListener('activate', function(e) {
     })
   );
 });
+
+// fetch data from the service worker and use it
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+        
+        // if cache exists, respond
+        if (request) {
+            console.log('responding with cache : ' + e.request.url)
+            return request;
+        } 
+        
+        // if none exists, respond with fetch
+        else {
+            console.log('file is not cached, fetching : ' + e.request.url)
+            return fetch(e.request);
+        }
+    })
+  )
+});
